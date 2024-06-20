91a664f029467f0edaad77ed4e251b10b36f4882
Camilo Adama <u202117587@upc.edu.pe>
2024-06-20 02:36:14 -0500

    SE MODIFICO LANDING
    ty

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-
+  constructor(private viewportScroller: ViewportScroller) { }
+  /*NAVBAR*/
+  scrollToSection(id: string) {
+    this.viewportScroller.scrollToAnchor(id);
+  }
 }
